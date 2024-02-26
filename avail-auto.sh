a21df8bcc93ff1541923575508e2020f1c9f5ca7
dragonz <chitocrypto@gmail.com>
2024-02-27 02:15:11 +0800

    upgrade to v1.11.0.0

--- avail-auto.sh
+++ avail-auto.sh
@@ -11,6 +11,8 @@ echo -e "\e[0m"
 
 sleep 2
 
+ufw allow 30333
+
 # set vars
 AVAIL_PORT=30333
 if [ ! $NODENAME ]; then
@@ -47,7 +49,7 @@ echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 git clone https://github.com/availproject/avail.git
 cd avail
 mkdir -p data
-git checkout v1.9.0.0
+git checkout v1.11.0.0
 cargo build --release -p data-avail
 . $HOME/.bash_profile
 sudo cp $HOME/avail/target/release/data-avail /usr/local/bin
